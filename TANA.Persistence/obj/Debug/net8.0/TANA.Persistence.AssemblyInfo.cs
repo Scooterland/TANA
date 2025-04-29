17b7f9b349344742c1a1553644a5e7e89bd9dc5d
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2025-04-29 11:09:39 +0200

    test

--- TANA.Persistence/obj/Debug/net8.0/TANA.Persistence.AssemblyInfo.cs
+++ TANA.Persistence/obj/Debug/net8.0/TANA.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925761520022cf2adf989a0acc6444fa5e68c634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5c492170cf17d4667fcb06132b3d89bf13888f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
