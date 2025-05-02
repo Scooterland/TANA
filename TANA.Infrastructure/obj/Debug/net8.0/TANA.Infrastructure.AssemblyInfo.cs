16f8135561c3b4036c4d1b0bdc73f334ffd44cba
Karina Kristensen <karinakristensen2@gmail.com>
2025-05-02 15:42:13 +0200

    ikke noget

--- TANA.Infrastructure/obj/Debug/net8.0/TANA.Infrastructure.AssemblyInfo.cs
+++ TANA.Infrastructure/obj/Debug/net8.0/TANA.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5304029db456626565b6294062d884ed524d58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126526ba1df785549411fcce794c89af8026cdfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
