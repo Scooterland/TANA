be5304029db456626565b6294062d884ed524d58
ThomasWin <tomner01@gmail.com>
2025-05-02 12:50:49 +0200

    Fix på PDF Generering.

--- TANA.Domain/obj/Debug/net8.0/TANA.Domain.AssemblyInfo.cs
+++ TANA.Domain/obj/Debug/net8.0/TANA.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8a50b7a95c3aa670c656526d98e5c328e58ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb77124f0d3b5b30a9fd65b0255bbb3dd8ca78c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
