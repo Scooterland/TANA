e060777850ebc86d2b3be658a0bc409da95058f6
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2025-05-02 10:05:39 +0200

    ....

--- TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
+++ TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925761520022cf2adf989a0acc6444fa5e68c634")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bccaa45b7b8876367c4a9140a677819ac097330")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TANA.Domain/obj/Debug/net8.0/TANA.Domain.AssemblyInfo.cs
+++ TANA.Domain/obj/Debug/net8.0/TANA.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8a54f00a452360f0fc86ac503fc3a31be0bd1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bccaa45b7b8876367c4a9140a677819ac097330")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
