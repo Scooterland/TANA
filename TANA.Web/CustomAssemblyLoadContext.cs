a13fc0e9ca85fc044ddd46c7f44b74b1d8b212e7
ThomasWin <tomner01@gmail.com>
2025-05-21 16:48:05 +0200

    Revert til før docker

--- TANA.Web/CustomAssemblyLoadContext.cs
+++ TANA.Web/CustomAssemblyLoadContext.cs
@@ -1,34 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
-namespace TANA.Web
+public class CustomAssemblyLoadContext : AssemblyLoadContext
 {
-    public class CustomAssemblyLoadContext : AssemblyLoadContext
+    public IntPtr LoadUnmanagedLibrary(string absolutePath)
     {
-        public IntPtr LoadUnmanagedLibrary(string absolutePath)
-        {
-            return LoadUnmanagedDll(absolutePath);
-        }
+        return LoadUnmanagedDll(absolutePath);
+    }
 
-        protected override IntPtr LoadUnmanagedDll(string unmanagedDllPath)
-        {
-            return LoadUnmanagedDllFromPath(unmanagedDllPath);
-        }
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllPath)
+    {
+        return LoadUnmanagedDllFromPath(unmanagedDllPath);
+    }
 
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            return null!;
-        }
-
-        public static string GetWkhtmltoxLibraryPath()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                return "libwkhtmltox.dll";
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return "/usr/lib/libwkhtmltox.so";
-
-            throw new PlatformNotSupportedException("Unsupported OS");
-        }
+    protected override Assembly Load(AssemblyName assemblyName)
+    {
+        return null!;
     }
 }
--- TANA.Web/Program.cs
+++ TANA.Web/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.InteropServices;
 using TANA.Application.Services;
 using TANA.Domain.Interface;
 using TANA.Infrastructure.Services;
@@ -10,11 +9,13 @@ using TANA.Persistence.Data;
 using TANA.Persistence.Repositories;
 using TANA.Web.Authentication;
 using TANA.Web.Components;
-using TANA.Web;
 using TANA.Application.Interfaces;
 using TANA.Domain.Repositories;
+using TANA.Web;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // ✅ Razor components & Blazor Server
 builder.Services.AddRazorComponents()
@@ -27,8 +28,8 @@ builder.Services.AddServerSideBlazor();
 // ✅ Authentication & Authorization
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<ProtectedSessionStorage>();
-builder.Services.AddScoped<AuthenticationStateProvider, SessionAuthenticationStateProvider>();
-
+builder.Services.AddScoped<AuthenticationStateProvider,
+                           SessionAuthenticationStateProvider>();
 // ✅ Application Services
 builder.Services.AddScoped<IBrugerRepository, BrugerRepository>();
 builder.Services.AddScoped<IKundeRepository, KundeRepository>();
@@ -42,26 +43,17 @@ builder.Services.AddScoped<IRejseService, RejseService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IEmailSettingsService, EmailSettingsService>();
 builder.Services.AddScoped<ITravelPlanService, TravelPlanService>();
-
+var context = new CustomAssemblyLoadContext();
+context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "libwkhtmltox", "libwkhtmltox.dll"));
 builder.Services.AddScoped<TemplateStateService>();
 builder.Services.AddScoped<ITemplateRepository, TemplateRepository>();
 builder.Services.AddScoped<TemplateLibraryService>();
 
-// ✅ Load native wkhtmltox library based on OS
-var context = new CustomAssemblyLoadContext();
-
-var libPath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-    ? Path.Combine(Directory.GetCurrentDirectory(), "libwkhtmltox", "libwkhtmltox.dll")
-    : "/usr/lib/libwkhtmltox.so";
-
-Console.WriteLine($"[Startup] Loading wkhtmltox from: {libPath}");
-context.LoadUnmanagedLibrary(libPath);
 
 // ✅ Database Context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// ✅ HTTP Client (adjust if needed)
 builder.Services.AddHttpClient("TanaApi", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5000/");
