82ff133791d78329b8d11773ed8fa06dd62712d5
Karina Kristensen <karinakristensen2@gmail.com>
2025-05-23 12:13:20 +0200

    Fixet API'er, nu virker de også i postman

--- TANA.API/Program.cs
+++ TANA.API/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
+using TANA.Application.Interfaces;
+using TANA.Application.Services;
+using TANA.Domain.Interface;
+using TANA.Domain.Repositories;
+using TANA.Infrastructure.Services;
+using TANA.Persistence.Data;
+using TANA.Persistence.Repositories;
+using TANA.Web.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +18,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBrugerRepository, BrugerRepository>();
+builder.Services.AddScoped<IKundeRepository, KundeRepository>();
+builder.Services.AddScoped<IRejseRepository, RejseRepository>();
+builder.Services.AddScoped<ITurRepository, TurRepository>();
+
+builder.Services.AddScoped<IBrugerService, BrugerService>();
+builder.Services.AddScoped<ITurService, TurService>();
+builder.Services.AddScoped<IKundeService, KundeService>();
+builder.Services.AddScoped<IRejseService, RejseService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IEmailSettingsService, EmailSettingsService>();
+builder.Services.AddScoped<ITravelPlanService, TravelPlanService>();
+builder.Services.AddScoped<TemplateStateService>();
+builder.Services.AddScoped<ITemplateRepository, TemplateRepository>();
+builder.Services.AddScoped<TemplateLibraryService>();
+
+builder.Services.AddHttpClient("TanaApi", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/");
+});
 
 var app = builder.Build();
 
