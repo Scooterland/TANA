ac02d0f55f6cf43223fb4c01722b72cc75f7680e
AmjadRn <165997348+AmjadRn@users.noreply.github.com>
2025-05-26 23:59:30 +0200

    Add QuestPDF.Settings.License og TemplatePdfService

--- TANA.Web/Program.cs
+++ TANA.Web/Program.cs
@@ -75,6 +75,7 @@ builder.Services.AddScoped<ITravelPlanService, TravelPlanService>();
 builder.Services.AddScoped<TemplateStateService>();
 builder.Services.AddScoped<ITemplateRepository, TemplateRepository>();
 builder.Services.AddScoped<TemplateLibraryService>();
+builder.Services.AddScoped<TemplatePdfService>();
 
 // ✅ Database Context
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -84,6 +85,7 @@ builder.Services.AddHttpClient("TanaApi", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5000/");
 });
+QuestPDF.Settings.License = LicenseType.Community;
 
 var app = builder.Build();
 
