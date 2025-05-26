80e9eae9af5b01c4e93aa86f7f0af42fd3509c8f
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2025-05-27 00:59:23 +0200

    Nogle få ændringer

--- TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
+++ TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274de6c3e73f3a056daa1bba11b578a6c3f681db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ad30d2d925f92c19018356772fdefb6e39bd44")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
