47424f24140c4ca37d5ce007a63569345f6b0f57
Karina Kristensen <karinakristensen2@gmail.com>
2025-05-27 09:21:51 +0200

    ...

--- TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
+++ TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee801e281b5b9c0585a087767edf030b59cf6c2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb16951acc1a69454a0a06cfb8ae82b44c36997")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
