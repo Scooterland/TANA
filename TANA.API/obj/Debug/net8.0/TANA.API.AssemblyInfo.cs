62525f74433c1a7e11393dd4b69bd516d9e1af92
Karina Kristensen <karinakristensen2@gmail.com>
2025-06-10 12:56:56 +0200

    xUnit test på BrugerService

--- TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
+++ TANA.API/obj/Debug/net8.0/TANA.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47424f24140c4ca37d5ce007a63569345f6b0f57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf252aa9cf42c66d997cd63102f70f3055d955a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TANA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
